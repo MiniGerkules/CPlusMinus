29c4271aeaa3872c088cf058f4469878dfdcd263
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-10-16 21:18:17 +0300

    Add lexical analyzer class

---
+++ src/main/kotlin/lexer/Lexer.kt
@@ -0,0 +1,23 @@
+package lexer
+
+import tokens.TokenType
+
+/**
+ * The class representing the lexical analyzer of the Jer language.
+ *
+ * @property pathToFileWithCode path to source code file
+ * @property currentPosition the current position of the lexer in the code
+ * @property tokensList list of all possible language tokens
+ */
+class Lexer(val pathToFileWithCode: String) {
+    var currentPosition: Int = 0
+    var tokensList: MutableList<TokenType> = mutableListOf()
+}
+
+
+
+
+
+
+
+
