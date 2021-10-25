36707d31538373317f1e9080774244758462046e
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-10-26 00:08:29 +0300

    Update the kotlin doc

--- src/main/kotlin/tokens/Token.kt
+++ src/main/kotlin/tokens/Token.kt
@@ -2,6 +2,10 @@ package tokens
 
 /**
  * The class describing the language token
+ *
+ * @property type the type of token
+ * @property text the text of the token
+ * @property position the position of the token in the source code
  */
-class Token(val type: TokenType, val text: String) {
+class Token(val type: TokenType, val text: String, val position: Int) {
 }
