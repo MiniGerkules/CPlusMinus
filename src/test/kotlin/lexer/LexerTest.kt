9ad82b588dc758e8a52d6f7879db4b3a039f5a2f
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-11-04 12:45:29 +0300

    Create the lexer test structure.

--- src/test/kotlin/lexer/LexerTest.kt
+++ src/test/kotlin/lexer/LexerTest.kt
@@ -1,11 +1,83 @@
 package lexer
 
-import org.junit.Test
+import java.io.File
+
 import org.junit.Assert.*
+import org.junit.Test
+import tokens.*
 
+/**
+ * The class that tests the lexer of the C+- language
+ *
+ * @property workingDir the set of folders that create a path
+ * to the working directory
+ */
 class LexerTest {
+    private val workingDir: Array<String> = arrayOf("..", "..", "..", "main", "resources")
 
+    /**
+     * The method creates the path to the [workingDir]
+     *
+     * @param fileName the name of the file with the C+- code
+     * @return the path to the [fileName] in [workingDir] in your OS
+     */
+    private fun getFilePath(fileName: String): String {
+        val path: StringBuilder = StringBuilder()
+
+        for (part in workingDir)
+            path.append(part).append(File.separator)
+
+        return path.append(fileName).toString()
+    }
+
+    /**
+     * The method that checks equals expected result and result that returns by lexer
+     *
+     * @param path the path to file with the C+- code
+     * @param absRes expected result
+     */
+    private fun checker(path: String, absRes: List<TokenType>) {
+        val lexer = Lexer(path)
+
+        try {
+            val methodRes = lexer.lexicalAnalysis()
+
+            for (i in methodRes.indices)
+                assertEquals(methodRes[i].type.toString(), absRes[i].toString())
+        } catch (error: Exception) {
+            fail(error.message)
+        }
+    }
+
+    /**
+     * The test tests the work lexer using the simple example. The source
+     * data is stored in file resources/simpleCode.txt
+     */
     @Test
-    fun lexicalAnalysis() {
+    fun `test lexical analysis`() {
+        val path: String = getFilePath("simpleCode.txt")
+
+        // The result that should be
+        val absRes: List<TokenType> = listOf(
+            Int32(), Identifier() // Add the following types
+        )
+
+        checker(path, absRes)
+    }
+
+    /**
+     * The test tests the recognition of primitive types. The source
+     * data is stored in file resources/primitiveTypes.txt
+     */
+    @Test
+    fun `test recognition of primitive types`() {
+        val path: String = getFilePath("primitiveTypes.txt")
+
+        // The result that should be
+        val absRes: List<TokenType> = listOf(
+            // Add expected types
+        )
+
+        checker(path, absRes)
     }
 }
