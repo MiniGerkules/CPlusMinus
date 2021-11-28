c7ac19e6e523426bd6c41ffcf98ff2074879e9da
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-11-28 12:18:59 +0300

    Add a field currentNode and update functions match and require

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -1,16 +1,18 @@
 package parser
 
-import tokens.Token
-import tokens.TokenType
+import parser.ast.*
+import tokens.*
 
 /**
  * Parser of the C+- language
  *
  * @property tokens list of all tokens that meet in the code
  * @property currentIndex the current index of the parser in the [tokens]
+ * @property currentNode the current node, which we will fill with child nodes
  */
 class Parser(private val tokens: List<Token>) {
     private var currentIndex: Int = 0
+    private lateinit var currentNode: ExpressionNode
 
     /**
      * The method is checks the current token type to coincide with
@@ -21,8 +23,8 @@ class Parser(private val tokens: List<Token>) {
      */
     private fun match(possibleTokenTypes: List<TokenType>): Token? {
         if (currentIndex < tokens.size)
-            if (possibleTokenTypes.any { it.toString() == tokens[currentIndex].type.toString() })
-                return tokens[currentIndex++]
+            if (possibleTokenTypes.any { it::class == tokens[currentIndex]::class })
+                return tokens[currentIndex]
 
         return null
     }
@@ -31,18 +33,47 @@ class Parser(private val tokens: List<Token>) {
      * The method is checks the current token type to coincide with
      * possible token types
      *
-     * @return the token
+     * @return returns the required token and increases [currentIndex] by 1
+     * @throws IllegalArgumentException if the required token type wasn't
+     * found. In this case, the [currentIndex] increase doesn't occur
      */
     private fun require(possibleTokenTypes: List<TokenType>): Token {
-        return match(possibleTokenTypes) ?:
-            throw IllegalArgumentException("Error!!! Expected: $possibleTokenTypes. " +
-                    "Actual ${tokens[currentIndex].type}")
+        val temp = match(possibleTokenTypes)
+
+        if (temp != null) {
+            ++currentIndex
+            return temp
+        } else {
+            throw IllegalArgumentException("Error!!! Expected: $possibleTokenTypes " +
+                    "actual: ${tokens[currentIndex].type} on position " +
+                    "${tokens[currentIndex].position}")
+        }
+    }
+
+    /**
+     * The function parses the expression of C+- language
+     */
+    private fun parseExpression() {
+        // Expression can start from type or identifier
+        val temp = match(listOf(Identifier()) + PrimitiveType.types)
+            ?: throw error("A type or identifier is expected at the beginning of an expression!" +
+                           " Error on position ${tokens[currentIndex].position}")
     }
 
     /**
      * The method that starts parsing the C+- code
+     *
+     * @return the root of the AST
      */
-    fun parseCode() {
-        error("Not implement!")
+    fun parseCode(): ExpressionNode {
+        val start = MainFunNode()
+        currentNode = start
+
+        while (currentIndex < tokens.size) {
+            parseExpression()
+            require(listOf(ExpEnd()))
+        }
+
+        return start
     }
 }
