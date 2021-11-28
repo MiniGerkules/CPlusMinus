c6f695dd19501305d274538b204ff9d8af24a53b
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-11-28 12:16:10 +0300

    Add a static list with all primitive types

--- src/main/kotlin/tokens/PrimitiveType.kt
+++ src/main/kotlin/tokens/PrimitiveType.kt
@@ -5,4 +5,7 @@ package tokens
  * such as int32, float32, char and void
  */
 sealed class PrimitiveType: TokenType() {
+    companion object {
+        val types: List<TokenType> = listOf(Int32(), Float32(), Char(), Void())
+    }
 }
