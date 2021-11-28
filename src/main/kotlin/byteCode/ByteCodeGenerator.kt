8efa86011c1df41ca94febdc5dd1dc8ac5aabe97
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-11-28 21:13:30 +0300

    Add the class that generates the bytecode for JVM

--- src/main/kotlin/byteCode/ByteCodeGenerator.kt
+++ src/main/kotlin/byteCode/ByteCodeGenerator.kt
@@ -1,4 +1,16 @@
 package byteCode
 
-class ByteCodeGenerator {
+import parser.ast.*
+
+/**
+ * The class generates bytecode for the JVM on the abstract syntax tree.
+ * The tree is built for the source code for the C+- language
+ */
+class ByteCodeGenerator(private val rootNode: MainFunNode) {
+    /**
+     * The method starts bytecode generation
+     */
+    fun generateByteCode() {
+
+    }
 }
