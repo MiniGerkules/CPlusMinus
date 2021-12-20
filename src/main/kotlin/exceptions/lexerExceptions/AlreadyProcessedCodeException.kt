8680f45cbd41898a9d60e6462610194be2fba22f
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-12-20 22:06:47 +0300

    Delete unused exception

--- src/main/kotlin/exceptions/lexerExceptions/AlreadyProcessedCodeException.kt
+++
@@ -1,6 +0,0 @@
-package exceptions.lexerExceptions
-
-/**
- * The exception that occurs when trying to re-process the processed code
- */
-class AlreadyProcessedCodeException(message: String) : Exception(message)
