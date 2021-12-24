9b747aeb98f30987785a57e62039fc99868e75d3
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-12-24 23:48:31 +0300

    Create the base classes for lexer and parser exceptions

---
+++ src/main/kotlin/exceptions/lexerExceptions/LexerException.kt
@@ -0,0 +1,8 @@
+package exceptions.lexerExceptions
+
+/**
+ * Base class for all lexer exceptions
+ *
+ * @param message exception informational message
+ */
+open class LexerException(message: String) : Exception(message)
---
+++ src/main/kotlin/exceptions/parserExceptions/ParserException.kt
@@ -0,0 +1,8 @@
+package exceptions.parserExceptions
+
+/**
+ * Base class for all parser exceptions
+ *
+ * @param message exception informational message
+ */
+open class ParserException(message: String) : Exception(message)
