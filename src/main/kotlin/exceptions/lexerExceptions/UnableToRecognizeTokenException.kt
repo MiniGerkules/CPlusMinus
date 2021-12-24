1511c3142a1f771b8b1afbe1517a02e9f35c2303
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-12-24 23:51:03 +0300

    Update lexer exception

--- src/main/kotlin/exceptions/lexerExceptions/UnableToRecognizeTokenException.kt
+++ src/main/kotlin/exceptions/lexerExceptions/UnableToRecognizeTokenException.kt
@@ -1,8 +1,8 @@
 package exceptions.lexerExceptions
 
 /**
+ * The exception that is thrown when the lexer cannot recognize the language token
  *
+ * @param message exception informational message
  */
-class UnableToRecognizeTokenException(message: String) : Exception(message) {
-    constructor(): this("")
-}
+class UnableToRecognizeTokenException(message: String) : LexerException(message)
