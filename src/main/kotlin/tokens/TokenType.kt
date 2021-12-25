6295fd520cffead7d939b59927a6f9f5523f3dde
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-12-25 16:10:13 +0300

    Add methods equals and hashcode

--- src/main/kotlin/tokens/TokenType.kt
+++ src/main/kotlin/tokens/TokenType.kt
@@ -7,4 +7,25 @@ package tokens
  */
 abstract class TokenType {
     abstract val regex: Regex
+
+    /**
+     * Method comparing class objects
+     */
+    override fun equals(other: Any?): Boolean {
+        return if (other == null)
+            false
+        else if (this === other)
+            true
+        else {
+            if (other is TokenType)
+                this.regex.pattern == other.regex.pattern
+            else
+                false
+        }
+    }
+
+    /**
+     * Method that generates a hash code for an object
+     */
+    override fun hashCode(): Int = regex.pattern.hashCode()
 }
