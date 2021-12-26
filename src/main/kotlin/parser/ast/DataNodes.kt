cddfe1864afdf882d6101977d5aa6fe6156beba3
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-12-26 14:06:51 +0300

    Add a new AST node

--- src/main/kotlin/parser/ast/DataNodes.kt
+++ src/main/kotlin/parser/ast/DataNodes.kt
@@ -16,3 +16,10 @@ class NumberNode(val number: Token): ASTNode()
  * @property variable the token of type "Variable"
  */
 class VariableNode(val type: Token, val variable: Token): ASTNode()
+
+/**
+ * The class describes an AST node that represents a string or character value
+ *
+ * @param string token containing a string or character
+ */
+class StringNode(val string: Token): ASTNode()
